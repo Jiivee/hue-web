8504a7726ad38a9e4bb0e521f9d7e5ece43e2cda
Jiivee <joni.vayrynen@gmail.com>
2016-03-17 16:07:49 +0200

    Change saturation and brightness as well

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,16 +10,26 @@
 angular.module('hueWebApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.test = 'aaaaaa';
-    /*
+
     $http.get('http://localhost:3003/users').success(function(data) {
-      console.log(data);
+      console.log(data.state);
+      $scope.hue = data.state.hue;
+      $scope.saturation = data.state.sat;
+      $scope.brightness = data.state.bri;
+
     });
-    */
+
     $scope.hue = 0;
+    $scope.saturation = 0;
+    $scope.brightness = 0;
 
     $scope.setColor = function() {
       console.log('setColor');
-      var data = {hue: $scope.hue};
+      var data = {
+        hue: $scope.hue,
+        brightness: $scope.brightness,
+        saturation: $scope.saturation
+      };
       console.log(data);
       var request = $http({
         method: 'post',
