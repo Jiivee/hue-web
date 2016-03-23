89f61d1495ee95c202d2ad112f39e30db80c3bd0
Jiivee <joni.vayrynen@gmail.com>
2016-03-23 17:11:51 +0200

    changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,11 +11,25 @@ angular.module('hueWebApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.test = 'aaaaaa';
 
-    $http.get('http://localhost:3003/users').success(function(data) {
+    $http.get('http://localhost:3003/lights').success(function(data) {
       console.log(data.state);
       $scope.hue = data.state.hue;
       $scope.saturation = data.state.sat;
       $scope.brightness = data.state.bri;
+
+    });
+    $scope.test = 90;
+
+    $http.get('http://localhost:3003/lights/status/all').success(function(data) {
+      console.log(data[1]);
+      var lamps = data;
+      $scope.activeLamps = [];
+      for (var lamp in lamps) {
+        if (lamps[lamp].state.reachable === true) {
+          $scope.activeLamps.push(lamps[lamp]);
+        }
+      }
+      console.log($scope.activeLamps);
 
     });
 
@@ -33,7 +47,7 @@ angular.module('hueWebApp')
       console.log(data);
       var request = $http({
         method: 'post',
-        url: 'http://localhost:3003/users',
+        url: 'http://localhost:3003/lights',
         data: data
       });
 
@@ -43,5 +57,25 @@ angular.module('hueWebApp')
         }
       );
     };
+
+    $scope.$watch('hue', function() {
+      var data = {
+        hue: $scope.hue,
+        brightness: $scope.brightness,
+        saturation: $scope.saturation
+      };
+      console.log(data);
+      var request = $http({
+        method: 'post',
+        url: 'http://localhost:3003/lights',
+        data: data
+      });
+
+      request.success(
+        function(response) {
+          console.log(response);
+        }
+      );
+    }, true);
 
   });
